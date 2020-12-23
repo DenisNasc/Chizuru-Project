4046373e976d807f8203ac4a5f8a12ab3ca0efb4
Denis <devdenisbr@gmail.com>
2020-12-23 09:42:29 -0300

    Added React Helmet

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -12,7 +12,7 @@ const App: React.FC = () => {
         <AppContextProvider>
             <UserContextProvider>
                 <StyleProvider>
-                    <Routes />;
+                    <Routes />
                 </StyleProvider>
             </UserContextProvider>
         </AppContextProvider>
--- src/styles/templates/index.tsx
+++ src/styles/templates/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 
+import Helmet from "components/Helmet";
 import Header from "components/Header";
 
 const DefaultTemplate: React.FC = ({ children }) => {
     return (
         <>
+            <Helmet title="Chizuru Project" />
             <Header />
             {children}
         </>
